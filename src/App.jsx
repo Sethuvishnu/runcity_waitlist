7e3994439d3776b9c1d8984bf3e229eb9bda4fd5
Sethuvishnu <vishnuitsme100@gmail.com>
2026-03-15 22:25:57 +0530

    added new main logo and updated app

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import { FaInstagram } from "react-icons/fa";
 import ResultScroller from "./ResultScroller";
-import logo from "./assets/logo_fav.png";
+import logo from "./assets/logo_main.png";
 
 function App() {
   const [email, setEmail] = useState("");
