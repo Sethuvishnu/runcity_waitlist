5a4175aa9923a44798ecee03d5da8a9effd08c08
Sethuvishnu <vishnuitsme100@gmail.com>
2026-06-19 21:45:27 +0530

    jason

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ const admin = require("firebase-admin");
 
 
 // ── Firebase Admin ──────────────────────────────────────────────────
-const serviceAccount = require("./serviceAccount.json"); // <-- uses local file
+// const serviceAccount = require("./serviceAccount.json"); // <-- uses local file
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
